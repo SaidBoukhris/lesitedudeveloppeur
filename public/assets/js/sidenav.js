118354e5544b8478d21aacbce7b1fe557e578bee
said <saidboukhris.pro@gmail.com>
2021-04-18 00:16:44 +0200

    #6 refactoring navbar issues

--- public/assets/js/sidenav.js
+++ public/assets/js/sidenav.js
@@ -1,7 +1,7 @@
 function openNav() {
     document.getElementById("sidenav").style.width = "200px";
     document.getElementById("main").style.marginLeft = "200px";
-    document.getElementById("header").style.marginLeft = "200px";
+    document.getElementById("header").style.marginLeft = "0px";
 }
 
 function closeNav() {
