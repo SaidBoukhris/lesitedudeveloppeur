55a7f42972c78e65263ec92fa80fd7b4f4c5f237
said <saidboukhris.pro@gmail.com>
2021-04-23 23:13:51 +0200

    #10 Add pagination to Article

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use App\Entity\Article;
 use App\Repository\ArticleRepository;
 use App\Repository\CategoriesRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -17,10 +19,17 @@ class ArticleController extends AbstractController
     /**
      * @Route("/", name="accueil")
      */
-    public function index(ArticleRepository $articles,CategoriesRepository $categories): Response
+    public function index(ArticleRepository $articles,CategoriesRepository $categories,PaginatorInterface $paginator,Request $request): Response
     {
+        // Méthode findBy qui permet de récupérer les données avec des critères de filtre et de tri
+        $donnees = $this->getDoctrine()->getRepository(Article::class)->findBy([],['createdAt' => 'desc']);
+        $articles = $paginator->paginate(
+            $donnees, // Requête contenant les données à paginer (ici nos articles)
+            $request->query->getInt('page', 1), // Numéro de la page en cours, passé dans l'URL, 1 si aucune page
+            2 // Nombre de résultats par page
+        );
         return $this->render('article/index.html.twig', [
-            'articles' => $articles->findBy([],['createdAt'=> 'DESC']),
+            'articles' => $articles,
             'controller_name' => 'Les Articles',
             'categories' => $categories->findAll()
         ]);
