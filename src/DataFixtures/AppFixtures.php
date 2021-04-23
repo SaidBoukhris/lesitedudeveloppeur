7b6f5e97b51bb782621a616dccb866a45fdbee00
said <saidboukhris.pro@gmail.com>
2021-04-24 00:58:10 +0200

    #11 Add style pagination /accueil

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -25,24 +25,34 @@ class AppFixtures extends Fixture
                     -> setBanner("https://images.unsplash.com/photo-1530228648323-ffb9f3f8f2c4?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fGJhbm5lcnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=60");
         $manager    -> persist($user);   
         }
-        for($j = 1 ; $j <= 3 ; $j++) {
+        for($j = 1 ; $j <= 10 ; $j++) {
             $categorie = new Categories();
             $categorie  -> setName("categorie ".$j);
             $manager    -> persist($categorie);
-            for($k = 1 ; $k <= 2 ; $k++) {
+            for($k = 1 ; $k <= 10 ; $k++) {
                 $article = new Article();
                 $article -> setTitle("Titre article 0".$k)
                         -> setContent("
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni ut doloribus repudiandae pariatur quam nemo maiores obcaecati, velit corporis iste laboriosam saepe, ipsum totam voluptas facilis eos fugit in deleniti!
-                            Quam voluptatum nam nisi accusamus alias nulla, amet eum ea harum quas, quidem molestias perferendis exercitationem quod, est similique. Qui ducimus nam fuga magni odit possimus quis repellat atque animi.
-                            Deleniti corrupti labore dolorum, ipsa ea fugit commodi ipsum blanditiis sint quo iste soluta dolore nesciunt? Iure, quae totam possimus sint assumenda odio illo enim, incidunt quam consequatur voluptate aliquid!
-                            Nobis animi suscipit error, quae illo similique nihil earum, incidunt magnam perferendis debitis, consequuntur reiciendis doloribus veniam placeat. Earum maiores quisquam itaque illum id iure dolor corrupti amet nulla. Alias!
-                            Quis suscipit deleniti voluptates blanditiis possimus minima non. Porro veritatis perferendis voluptas illum similique fugit quibusdam cumque ipsum rem aspernatur aut, unde ad? Similique officiis, aut sunt iusto voluptatibus doloribus?"
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni ut doloribus repudiandae pariatur quam nemo maiores obcaecati, velit corporis iste laboriosam saepe, ipsum totam voluptas facilis eos fugit in deleniti!
+                        Quam voluptatum nam nisi accusamus alias nulla, amet eum ea harum quas, quidem molestias perferendis exercitationem quod, est similique. Qui ducimus nam fuga magni odit possimus quis repellat atque animi.
+                        Deleniti corrupti labore dolorum, ipsa ea fugit commodi ipsum blanditiis sint quo iste soluta dolore nesciunt? Iure, quae totam possimus sint assumenda odio illo enim, incidunt quam consequatur voluptate aliquid!
+                        Nobis animi suscipit error, quae illo similique nihil earum, incidunt magnam perferendis debitis, consequuntur reiciendis doloribus veniam placeat. Earum maiores quisquam itaque illum id iure dolor corrupti amet nulla. Alias!
+                        Quis suscipit deleniti voluptates blanditiis possimus minima non. Porro veritatis perferendis voluptas illum similique fugit quibusdam cumque ipsum rem aspernatur aut, unde ad? Similique officiis, aut sunt iusto voluptatibus doloribus?
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni ut doloribus repudiandae pariatur quam nemo maiores obcaecati, velit corporis iste laboriosam saepe, ipsum totam voluptas facilis eos fugit in deleniti!
+                        Quam voluptatum nam nisi accusamus alias nulla, amet eum ea harum quas, quidem molestias perferendis exercitationem quod, est similique. Qui ducimus nam fuga magni odit possimus quis repellat atque animi.
+                        Deleniti corrupti labore dolorum, ipsa ea fugit commodi ipsum blanditiis sint quo iste soluta dolore nesciunt? Iure, quae totam possimus sint assumenda odio illo enim, incidunt quam consequatur voluptate aliquid!
+                        Nobis animi suscipit error, quae illo similique nihil earum, incidunt magnam perferendis debitis, consequuntur reiciendis doloribus veniam placeat. Earum maiores quisquam itaque illum id iure dolor corrupti amet nulla. Alias!
+                        Quis suscipit deleniti voluptates blanditiis possimus minima non. Porro veritatis perferendis voluptas illum similique fugit quibusdam cumque ipsum rem aspernatur aut, unde ad? Similique officiis, aut sunt iusto voluptatibus doloribus?
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni ut doloribus repudiandae pariatur quam nemo maiores obcaecati, velit corporis iste laboriosam saepe, ipsum totam voluptas facilis eos fugit in deleniti!
+                        Quam voluptatum nam nisi accusamus alias nulla, amet eum ea harum quas, quidem molestias perferendis exercitationem quod, est similique. Qui ducimus nam fuga magni odit possimus quis repellat atque animi.
+                        Deleniti corrupti labore dolorum, ipsa ea fugit commodi ipsum blanditiis sint quo iste soluta dolore nesciunt? Iure, quae totam possimus sint assumenda odio illo enim, incidunt quam consequatur voluptate aliquid!
+                        Nobis animi suscipit error, quae illo similique nihil earum, incidunt magnam perferendis debitis, consequuntur reiciendis doloribus veniam placeat. Earum maiores quisquam itaque illum id iure dolor corrupti amet nulla. Alias!
+                        Quis suscipit deleniti voluptates blanditiis possimus minima non. Porro veritatis perferendis voluptas illum similique fugit quibusdam cumque ipsum rem aspernatur aut, unde ad? Similique officiis, aut sunt iusto voluptatibus doloribus?"
                         )
                         -> setThumb("https://images.unsplash.com/photo-1523726491678-bf852e717f6a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1050&q=80")
                         -> setActive(false)
-                        -> setLikes(100)
-                        -> setDislikes(3)
+                        -> setLikes(583)
+                        -> setDislikes(302)
                         -> setCategories($categorie)
                         -> setUsers($user);
                 $manager-> persist($article);
